e878b3878b892234aebe4d7f8a7ed0a5484ad4ed
Jason Stiebs <jason@rokkincat.com>
2015-10-26 10:22:13 -0500

    Making updates for heroku

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -3,7 +3,7 @@ defmodule Chat.UserSocket do
 
   channel "rooms:*", Chat.RoomChannel
 
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: :infinity
   transport :longpoll, Phoenix.Transports.LongPoll
 
   def connect(_params, socket) do
